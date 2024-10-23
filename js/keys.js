2b8a9fdfe062c98abd24c3bcbe6a1bec6fc4b359
The Wizard of Hahz <info@luvnft.com>
2024-10-23 06:14:39 -0400

    updated

--- js/keys.js
+++ js/keys.js
@@ -3,8 +3,8 @@
  * Please see the ReadMe file for details of where to obtain keys
  */
 
-const publish_key = '' //  ENTER YOUR PUBLISH KEY HERE
-const subscribe_key = '' //  ENTER YOUR SUBSCRIBE KEY HERE
+const publish_key = 'pub-c-4e8f00ab-e601-47db-830f-3bfb0f19c8d3' //  ENTER YOUR PUBLISH KEY HERE
+const subscribe_key = 'sub-c-bf4c3bd9-6978-41e7-95c5-5aed805c3cd5' //  ENTER YOUR SUBSCRIBE KEY HERE
 
 //  Feel free to modify this to use your own key - see the ReadMe for more information
-const GOOGLE_MAP_KEY = ''  //  ENTER GOOGLE API KEY WITH THE FOLLOWING APIS ENABLED: 'Maps Embed API', 'Maps JavaScript API'.
+const GOOGLE_MAP_KEY = 'AIzaSyB4T5-waGzFdVjDwtJ8Tk5yKfnc-fEsSuI'  //  ENTER GOOGLE API KEY WITH THE FOLLOWING APIS ENABLED: 'Maps Embed API', 'Maps JavaScript API'.
